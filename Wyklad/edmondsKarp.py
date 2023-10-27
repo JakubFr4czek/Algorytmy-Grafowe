c2d2996e3aa729038e52482b5abdf21124c49c01
JakubFr4czek <jakubfraczek.m8@gmail.com>
2023-10-27 15:22:30 +0200

    Kod z pierwszego wykladu

---
+++ Wyklad/edmondsKarp.py
@@ -0,0 +1,86 @@
+from collections import deque
+
+def BFS(G, v):
+
+    visited = [False for _ in range(len(G))]
+    path = [-1 for _ in range(len(G))]
+
+    queue = deque()
+
+    queue.append(v)
+    visited[v] = True
+
+    while len(queue) > 0:
+
+        temp = queue.popleft()
+
+        for i in range(len(G[temp])):
+
+            if G[temp][i] != 0 and visited[i] == False:
+
+                queue.append(i)
+                visited[i] = True
+                path[i] = temp
+
+    return path
+
+def EdmondsKarp(G, s, t):
+
+    #Wyszukuje sciezke powiekszajaca
+
+    path = BFS(G, s)
+
+    flow = 0
+
+    while(path[t] != -1): #dopóki istnieje ścieżka powiększająca
+
+        #teraz muszę znależć minimum w tej ścieżce powiększającej
+        minVal = float('inf')
+        curr = t
+
+        while(curr != s):
+
+            minVal = min(minVal, M[path[curr]][curr])
+            curr = path[curr]
+            #print(curr)
+
+        #teraz muszę znależć minimum w tej ścieżce powiększającej
+        curr = t
+        while(curr != s):
+
+            M[path[curr]][curr] -= minVal
+            M[curr][path[curr]] += minVal
+            curr = path[curr]
+
+        flow += minVal
+
+        path = BFS(G, s) #Wyszukuje sciezke powiekszajaca
+
+    return flow
+
+def ListToMatrixRepresentation( G ):
+
+    M = [ [0 for j in range(len(G))] for i in range(len(G))]
+
+    for i in range(len(G)):
+        for j in range(len(G[i])):
+            M[i][G[i][j][0]] = G[i][j][1]
+            M[G[i][j][0]][i] = 0
+
+    return M
+
+G = [
+
+    [(1,6), (4,3)],
+    [(2,2), (4,1), (5,2)],
+    [(3,4)],
+    [],
+    [(2,2), (5,1)],
+    [(2,2), (3,4)]
+
+
+]
+
+M = ListToMatrixRepresentation(G)
+
+print(EdmondsKarp(M, 0, 3))
